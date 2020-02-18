53f32e0af3608e5c7c658394d4b597397f28280f
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-02-18 09:08:45 +0200

    accepting city and converting to latitude and longitude, also showing the temperature

---
+++ utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+  const url ='https://api.darksky.net/forecast/38f9be1c2f954e51e19cb9afbda649fb/'+latitude+','+longitude
+  request({url:url, json: true}, (error, response)=> {
+    if(error){
+      callback('Unable to connect to weather services', undefined)
+    }else if (response.body.error){
+      callback('Unable to find location', undefined)
+    }else{
+      callback('It is currently '+response.body.currently.temperature+' out. There is a '+response.body.currently.cloudCover+' chance of rain.')
+    }
+  })
+}
+
+module.exports = forecast
